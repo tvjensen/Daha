38b48467bb1e9d6d614e6ec86ec71642a8030738
Olivier Moisan <olivier.p.moisan@gmail.com>
2018-06-23 15:09:44 -0700

    settings stuff

---
+++ Daha/PaymentInfoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PaymentInfoViewController.swift
+//  Daha
+//
+//  Created by omoisan on 6/23/18.
+//  Copyright © 2018 Thomas Jensen. All rights reserved.
+//
+
+import UIKit
+
+class PaymentInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
